7d09375add7502569e5d83752993a9fae54c6de7
Diego <Diego@Mendes>
2019-03-06 15:59:30 -0400

    projeto inicializado

---
+++ app/Client.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Client extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+    ];
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
+}
