31ee01edf57a356e0d58fa70ce27aaf714e22751
Diego <Diego@Mendes>
2019-03-06 16:58:38 -0400

    End point @index, @show, @store

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$app->group([
+    'prefix' => 'api/clients',
+    'namespace' => 'App\Http\Controller'
+].function () use($app){
+    
+    $app->get('', 'ClientsController@index');//trabalhando com coleção de dados.
+    $app->get('{id}', 'ClientsController@show');
+    $app->post('', 'ClientsController@store');
+    $app->post('id', 'ClientsController');
+});
